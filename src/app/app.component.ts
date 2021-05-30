ebb2f7e6335f503b1a8ad46f9545c125dd56a436
Camilo <juancmaringcu@gmail.com>
2021-05-30 15:42:58 -0500

    add and delete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,18 +7,28 @@ import {Empleado} from './modules/empleado'
 })
 export class AppComponent {
   empleadoArray: Empleado[] = [
-    {id: 1, name: "Camilo", city: "Bogotá"},
-    {id: 2, name: "Deyci", city: "Floridablanca"}
+    //{id: 1, name: "Camilo", city: "Bogotá"},
+    //{id: 2, name: "Deyci", city: "Floridablanca"}
   ];
 
   selectedEmpleado: Empleado = new Empleado();
-  
+
   addOrEdit(){
     if(this.selectedEmpleado.id === 0 ){
       this.selectedEmpleado.id = this.empleadoArray.length + 1
       this.empleadoArray.push(this.selectedEmpleado);
+      let arrayEmpl: Empleado[]= [];
+      if(localStorage.getItem('empleados') === null){
+        arrayEmpl.push(this.selectedEmpleado);
+        localStorage.setItem('empleados', JSON.stringify(arrayEmpl));
+      }else{
+        arrayEmpl =JSON.parse(localStorage.getItem('empleados'));
+        arrayEmpl.push(this.selectedEmpleado);
+        localStorage.setItem('empleados', JSON.stringify(arrayEmpl));
+      }
     }
     this.selectedEmpleado = new Empleado();
+    
   }
   openEdit(empleado: Empleado){
     this.selectedEmpleado = empleado;
@@ -27,6 +37,7 @@ export class AppComponent {
   delete(){
     if(confirm("Esta seguro?")){
       this.empleadoArray = this.empleadoArray.filter(x => x != this.selectedEmpleado);
+      localStorage.setItem('empleados', JSON.stringify(this.empleadoArray));
       this.selectedEmpleado = new Empleado();
     }
   }
