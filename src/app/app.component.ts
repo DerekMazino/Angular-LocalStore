c873194cfcc13a7d2bed4ae3fddb402a64a5d325
Camilo <juancmaringcu@gmail.com>
2021-05-30 13:44:03 -0500

    v1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
-
+import {Empleado} from './modules/empleado'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ejemploRegistroLogin';
+  empleadoArray: Empleado[] = [
+    {id: 1, name: "Camilo", city: "Bogotá"},
+    {id: 2, name: "Deyci", city: "Floridablanca"}
+  ];
+  selectedEmpleado: Empleado = new Empleado();
+  addOrEdit(){
+    if(this.selectedEmpleado.id === 0 ){
+      this.selectedEmpleado.id = this.empleadoArray.length + 1
+      this.empleadoArray.push(this.selectedEmpleado);
+    }
+    this.selectedEmpleado = new Empleado();
+  }
+  openEdit(empleado: Empleado){
+    this.selectedEmpleado = empleado;
+  }
+
+  delete(){
+    if(confirm("Esta seguro?")){
+      this.empleadoArray = this.empleadoArray.filter(x => x != this.selectedEmpleado);
+      this.selectedEmpleado = new Empleado();
+    }
+  }
+
 }
